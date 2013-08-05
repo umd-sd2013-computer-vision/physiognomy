f553bdb3d851f014b137ff659287418e075418eb
Preethi <preethipvg@gmail.com>
2013-08-05 11:53:34 -0400

    test

--- VisionTest/physiognomy/src/main/java/com/timetravellingtreasurechest/App.java
+++ VisionTest/physiognomy/src/main/java/com/timetravellingtreasurechest/App.java
@@ -1,13 +1,17 @@
-package com.timetravellingtreasurechest;
+ipackage com.timetravellingtreasurechest;
+
+import org.opencv.core.Point;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "We added something" );
+        System.out.println("One more line");  //
     }
 }
