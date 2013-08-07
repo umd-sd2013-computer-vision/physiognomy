1fcc176440882b4a633baaa8005f3862d36cf2d4
Allen_Polak <apolak@systemsinmotion.com>
2013-08-07 12:09:07 -0400

    Project restructuring

---
+++ vision/src/test/java/com/timetravellingtreasurechest/vision/AppTest.java
@@ -0,0 +1,16 @@
+package com.timetravellingtreasurechest.vision;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(JUnit4.class)
+public class AppTest {
+    @Test
+    public void testApp() {
+        App.main(null);
+    }
+}
