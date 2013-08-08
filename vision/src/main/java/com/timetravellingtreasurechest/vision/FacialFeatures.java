b42e48458b1bd1c48df02666da4a435d76e22103
Allen_Polak <apolak@systemsinmotion.com>
2013-08-08 14:51:36 -0400

    Port to maven based openCv

--- vision/src/main/java/com/timetravellingtreasurechest/vision/FacialFeatures.java
+++ vision/src/main/java/com/timetravellingtreasurechest/vision/FacialFeatures.java
@@ -3,17 +3,22 @@ package com.timetravellingtreasurechest.vision;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.highgui.Highgui;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.CascadeClassifier;
-import org.opencv.objdetect.Objdetect;
+import org.w3c.dom.css.Rect;
+
+import com.googlecode.javacv.*;
+import com.googlecode.javacpp.*;
+import com.googlecode.javacpp.properties.*;
+import com.googlecode.javacv.cpp.*;
+import com.googlecode.javacv.cpp.opencv_objdetect.CascadeClassifier;
+import static com.googlecode.javacv.cpp.opencv_core.*;
+import static com.googlecode.javacv.cpp.opencv_imgproc.*;
+import static com.googlecode.javacv.cpp.opencv_highgui.*;
+import static com.googlecode.javacv.cpp.opencv_imgproc.*;
+import com.googlecode.javacpp.annotation.*;
+import com.googlecode.javacv.procamcalib.*;
+import com.googlecode.javacv.procamcalib.icons.*;
+import com.googlecode.javacv.procamtracker.*;
+import com.googlecode.javacv.procamtracker.icons.*;
 
 public class FacialFeatures {
 	
@@ -26,19 +31,20 @@ public class FacialFeatures {
 	
 	private static final boolean OUTPUT_IMAGE = true; // output test image to file including boxes, only for testing. not included in final project
 	
-	private static CascadeClassifier face_cascade;
+	private 
+	static CascadeClassifier face_cascade;
 	private static CascadeClassifier leye_cascade;
 	private static CascadeClassifier reye_cascade;
 	private static CascadeClassifier mouth_cascade;
 	private static CascadeClassifier nose_cascade;
 	
-	private Mat image;
+	private CvMat image;
 	
-	private Rect face;
-	private Rect leye;
-	private Rect reye;
-	private Rect mouth;
-	private Rect nose;	
+	private CvRect face;
+	private CvRect leye;
+	private CvRect reye;
+	private CvRect mouth;
+	private CvRect nose;	
 	
     public static void main(String[] args) {   	
         for (int i = 0; i < args.length; i++) {
@@ -47,7 +53,7 @@ public class FacialFeatures {
     }
     
     public FacialFeatures(String imageFile) {
-    	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    	//System.loadLibrary(opencv_core.CV_NATIVE_LIBRARY_NAME);
     	
     	// load haarcascades xml files, doing this here to avoid link errors
     	if (face_cascade == null)
@@ -61,9 +67,9 @@ public class FacialFeatures {
     	if (nose_cascade == null)
     		nose_cascade = new CascadeClassifier("src\\haarcascades\\haarcascade_mcs_nose.xml");
     	
-    	image = Highgui.imread(imageFile, Highgui.CV_LOAD_IMAGE_COLOR);
+    	image = cvLoadImageM(imageFile, CV_LOAD_IMAGE_COLOR);
     	
-    	if (image.size().area() == 0) {
+    	if (image.size() == 0) {
 	        System.out.println("error: cannot open "+ imageFile + "\n");
 	        return;
     	}
@@ -75,7 +81,7 @@ public class FacialFeatures {
     	
     	//write image with boxes to file
     	if (OUTPUT_IMAGE)
-    		Highgui.imwrite("cv_" + imageFile, image);
+    		opencv_highgui.cvSaveImage("cv_" + imageFile, image);
         
         extractFeatures();
     }
@@ -86,29 +92,29 @@ public class FacialFeatures {
     
     private boolean findFeatures() {                 
         // opencv detection works 234234324x faster on grayscale images
-        Mat gray_image = new Mat();       
-        Imgproc.cvtColor(image, gray_image, Imgproc.COLOR_RGB2GRAY);
-        Imgproc.equalizeHist(gray_image, gray_image);        
+    	CvMat gray_image = new CvMat();       
+        cvCvtColor(image, gray_image, CV_RGB2GRAY);
+        cvEqualizeHist(gray_image, gray_image);        
 
         // detect face first to create roi to search for facial features, can greatly increase speed (esp for large images)
-        face = detectFeature(face_cascade, gray_image, new Size(gray_image.cols()/5, gray_image.rows()/5));
+        face = detectFeature(face_cascade, gray_image, new CvSize(gray_image.cols()/5, gray_image.rows()/5));
 
         if (face == null)
         	return false;
         
-        Rect faceROI = new Rect(face.x, face.y, face.width, face.height);
-        Mat face_crop_image = new Mat(gray_image, faceROI);
+        CvRect faceROI = new CvRect(face.x(), face.y(), face.width(), face.height());
+        CvMat face_crop_image = new CvMat(gray_image, faceROI);
 	
 		  // separate regions of interests to reduce search area
-		Rect upperFaceROI = new Rect(face.x,                face.y,                 face.width,   face.height/2);
-		Rect lowerFaceROI = new Rect(face.x,                face.y + face.height/2, face.width,   face.height/2);
-		Rect leftFaceROI =  new Rect(face.x,                face.y,                 face.width/2, face.height);
-		Rect rightFaceROI = new Rect(face.x + face.width/2, face.y,                 face.width/2, face.height);
+        CvRect upperFaceROI = new CvRect(face.x(),                face.y(),                 face.width(),   face.height()/2);
+		CvRect lowerFaceROI = new CvRect(face.x(),                face.y() + face.height()/2, face.width(),   face.height()/2);
+		CvRect leftFaceROI =  new CvRect(face.x(),                face.y(),                 face.width()/2, face.height());
+		CvRect rightFaceROI = new CvRect(face.x() + face.width()/2, face.y(),                 face.width()/2, face.height());
 			  
-		Mat upper_face_crop_image = new Mat(gray_image, upperFaceROI);
-		Mat lower_face_crop_image = new Mat(gray_image, lowerFaceROI);
-		Mat left_face_crop_image =  new Mat(gray_image, leftFaceROI);
-		Mat right_face_crop_image = new Mat(gray_image, rightFaceROI);
+		CvMat upper_face_crop_image = new CvMat(gray_image, upperFaceROI);
+		CvMat lower_face_crop_image = new CvMat(gray_image, lowerFaceROI);
+		CvMat left_face_crop_image =  new CvMat(gray_image, leftFaceROI);
+		CvMat right_face_crop_image = new CvMat(gray_image, rightFaceROI);
 		
 		if (LEYE)
 		    leye = detectFeature(leye_cascade, left_face_crop_image, null);
@@ -122,10 +128,10 @@ public class FacialFeatures {
 		
 		if (OUTPUT_IMAGE) {
 			putRects(image, face, 0, 0); 							// face rectangle
-			putRects(image, leye, face.x, face.y); 					// left eye rectangle
-			putRects(image, reye, face.x + face.width/2, face.y); 	// right eye rectangle
-		    putRects(image, mouth, face.x, face.y + face.height/2); // mouth rectangle
-		    putRects(image, nose, face.x, face.y); 					// nose rectangle
+			putRects(image, leye, face.x(), face.y()); 					// left eye rectangle
+			putRects(image, reye, face.x() + face.width()/2, face.y()); 	// right eye rectangle
+		    putRects(image, mouth, face.x(), face.y() + face.height()/2); // mouth rectangle
+		    putRects(image, nose, face.x(), face.y()); 					// nose rectangle
 		}
 		
 		return true;
@@ -134,17 +140,17 @@ public class FacialFeatures {
     // detect feature (indicated by casc parameter) from image
     // uses simple binary search to find a single object (with the minBoxes as the search term)
     // returns Rect object containing feature
-    private Rect detectFeature(CascadeClassifier casc, Mat image,  Size min) {
-    	MatOfRect rects = new MatOfRect();
-    	List<Rect> detected = new ArrayList<Rect>();
+    private CvRect detectFeature(CascadeClassifier casc, CvMat image,  CvSize min) {
+    	//CvMatOfRect rects = new CvMatOfRect();
+    	List<CvRect> detected = new ArrayList<CvRect>();
     	if (min == null)
-    		min = new Size(1,1);
+    		min = new CvSize(1,1);
     	
         for (int low = 3, high = 150, mid; detected.size() != 1 && low <= high;) {
           mid = (low + high) / 2;
-          casc.detectMultiScale(image, rects, 1.1, mid, Objdetect.CASCADE_SCALE_IMAGE, min, new Size(image.height(),image.width()));
+          casc.detectMultiScale(image, detected, 1.1, mid, Objdetect.CASCADE_SCALE_IMAGE, min, new CvSize(image.rows(),image.cols()));
           
-          detected = rects.toList();
+          //detected = rects.toList();
           if (detected.size() == 0)
             high = mid - 1;
           else if (detected.size() > 1)
@@ -158,13 +164,13 @@ public class FacialFeatures {
     
     // draw rects on image relative to offsets given (for roi placement)
     // only used for testing (if OUTPUT_IMAGE is set), not for final project
-    private void putRects(Mat image, Rect rect, int offset_x, int offset_y) {
+    private void putRects(CvMat image, CvRect rect, int offset_x, int offset_y) {
     	if (rect == null)
     		return;
     	
-	    Point pt1 = new Point(rect.x + rect.width + offset_x, rect.y + rect.height + offset_y);
-	    Point pt2 = new Point(rect.x + offset_x, rect.y + offset_y);
+    	CvPoint pt1 = new CvPoint(rect.x() + rect.width() + offset_x, rect.y() + rect.height() + offset_y);
+	    CvPoint pt2 = new CvPoint(rect.x() + offset_x, rect.y() + offset_y);
 	    
-	    Core.rectangle(image, pt1, pt2, new Scalar(0,255,0,0),1,8,0);
+	    cvRectangle(image, pt1, pt2, new CvScalar(0,255,0,0),1,8,0);
 	}
 }
