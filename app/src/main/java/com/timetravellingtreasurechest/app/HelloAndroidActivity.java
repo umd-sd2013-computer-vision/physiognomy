a8cae0d0054f0e2a7f7f2d1609ee62b528fe3426
Allen_Polak <apolak@systemsinmotion.com>
2013-08-15 13:35:18 -0400

    Added camera functionality, and backwards compatibility

--- app/src/main/java/com/timetravellingtreasurechest/app/HelloAndroidActivity.java
+++ app/src/main/java/com/timetravellingtreasurechest/app/HelloAndroidActivity.java
@@ -1,125 +1,77 @@
 package com.timetravellingtreasurechest.app;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import com.timetravellingtreasurechest.app.R;
+
+//import com.timetravellingtreasurechest.camera.*;
+
+//import com.timetravellingtreasurechest.physiognomy.ReportData;
+//import com.timetravellingtreasurechest.physiognomy.service.FacialFeatureService;
+//import com.timetravellingtreasurechest.physiognomy.service.IFacialFeatureService;
+//import com.timetravellingtreasurechest.physiognomy.service.IReportGeneratorService;
+//import com.timetravellingtreasurechest.physiognomy.service.ReportGeneratorService;
 
 public class HelloAndroidActivity extends Activity {
 
-	/**
-	 * Called when the activity is first created.
-	 * 
-	 * @param savedInstanceState
-	 *            If the activity is being re-initialized after previously being
-	 *            shut down then this Bundle contains the data it most recently
-	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
-	 *            is null.</b>
-	 */
+	Preview cameraSurfaceView;
+
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Setup the FrameLayout with the Camera Preview Screen
+		cameraSurfaceView = new Preview(this);
+		FrameLayout preview = (FrameLayout) this
+				.findViewById(R.id.PreviewFrame);
+		if (preview == null) {
+			System.out.println("PreviewFrame not found!");
+		} else {
+			System.out.println("PreviewFrame found");
+			preview.addView(cameraSurfaceView);
+		}
+
+		Button takeAPicture = (Button) this.findViewById(R.id.TakePhoto);
+		takeAPicture.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Camera camera = cameraSurfaceView.getCamera();
+				camera.takePicture(null, null, new HandlePictureStorage());
+			}
+		});
+
+	}
+
+	private class HandlePictureStorage implements PictureCallback {
+
+		@Override
+		public void onPictureTaken(byte[] picture, Camera camera) {
+			System.out.println("Picture successfully taken: " + picture);
+			// Intent myIntent = new Intent();
+			// myIntent.setClassName(HelloAndroidActivity.this,
+			// "com.timetravellingtreasurechest.gui.ManageReportActivity");
+			// ReportData report =
+			// reportGeneratorService.getReport(facialFeatureService.getFeatures(picture));
+			// myIntent.putExtra(MainActivity.REPORT, report);
+			// System.out.println("Starting new activity");
+			// startActivity(myIntent);
+			System.out.println("Activity returned");
+			cameraSurfaceView.startPreview();
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		new com.timetravellingtreasurechest.vision.FacialFeatures(
-		 (com.googlecode.javacv.cpp.opencv_core.CvMat) null);
-		getMenuInflater().inflate(
-				com.timetravellingtreasurechest.app.R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 }
-
-/*
- import android.content.Context;
- import android.graphics.Bitmap;
- import android.graphics.BitmapFactory;
- import android.graphics.Canvas;
- import android.graphics.Color;
- import android.graphics.Paint;
- import android.graphics.PointF;
- import android.media.FaceDetector;
- import android.media.FaceDetector.Face;
-// */
-//import android.app.Activity;
-//import android.os.Bundle;
-//import android.view.Menu;
-////import android.view.View;
-//
-//public class HelloAndroidActivity extends Activity {
-//
-//	@Override
-//	public void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		setContentView(com.timetravellingtreasurechest.app.R.layout.activity_main);
-//		// setContentView(new myView(this));
-//	}
-//
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		// new com.timetravellingtreasurechest.vision.FacialFeatures(
-//		// (com.googlecode.javacv.cpp.opencv_core.CvMat) null);
-//		getMenuInflater().inflate(
-//				com.timetravellingtreasurechest.app.R.menu.main, menu);
-//		return true;
-//	}
-//
-//	// private class myView extends View {
-//	//
-//	// private int imageWidth, imageHeight;
-//	// private int numberOfFace = 5;
-//	// private FaceDetector myFaceDetect;
-//	// private FaceDetector.Face[] myFace;
-//	// float myEyesDistance;
-//	// int numberOfFaceDetected;
-//	//
-//	// Bitmap myBitmap;
-//	//
-//	// public myView(Context context) {
-//	// super(context);
-//	//
-//	// /*BitmapFactory.Options BitmapFactoryOptionsbfo = new
-//	// BitmapFactory.Options();
-//	// BitmapFactoryOptionsbfo.inPreferredConfig = Bitmap.Config.zRGB_565;
-//	// myBitmap = BitmapFactory.decodeResource(getResources(),
-//	// com.timetravellingtreasurechest.app.R.drawable.face,
-//	// BitmapFactoryOptionsbfo);
-//	// imageWidth = myBitmap.getWidth();
-//	// imageHeight = myBitmap.getHeight();
-//	// myFace = new FaceDetector.Face[numberOfFace];
-//	// myFaceDetect = new FaceDetector(imageWidth, imageHeight,
-//	// numberOfFace);
-//	// numberOfFaceDetected = myFaceDetect.findFaces(myBitmap, myFace);*/
-//	//
-//	// }
-//	//
-//	// @Override
-//	// protected void onDraw(Canvas canvas) {
-//	//
-//	// canvas.drawBitmap(myBitmap, 0, 0, null);
-//	//
-//	// Paint myPaint = new Paint();
-//	// myPaint.setColor(Color.GREEN);
-//	// myPaint.setStyle(Paint.Style.STROKE);
-//	// myPaint.setStrokeWidth(3);
-//	//
-//	// for (int i = 0; i < numberOfFaceDetected; i++) {
-//	// Face face = myFace[i];
-//	// PointF myMidPoint = new PointF();
-//	// face.getMidPoint(myMidPoint);
-//	// myEyesDistance = face.eyesDistance();
-//	//
-//	// canvas.drawRect((int) (myMidPoint.x - myEyesDistance * 2),
-//	// (int) (myMidPoint.y - myEyesDistance * 2),
-//	// (int) (myMidPoint.x + myEyesDistance * 2),
-//	// (int) (myMidPoint.y + myEyesDistance * 2), myPaint);
-//	// }
-//	// }
-//	// }
-//
-//}
---
+++ app/src/main/java/com/timetravellingtreasurechest/app/Preview.java
@@ -0,0 +1,67 @@
+package com.timetravellingtreasurechest.app;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+
+public class Preview extends SurfaceView implements Callback {
+
+	private SurfaceHolder holder;
+    private Camera camera;
+	
+	@SuppressWarnings("deprecation")
+	public Preview(Context context) {
+		super(context);
+		
+		this.holder = this.getHolder();
+        this.holder.addCallback(this);
+        this.holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+        Camera.Parameters parameters = camera.getParameters();
+        parameters.setPreviewSize(width, height);
+        camera.setParameters(parameters);
+        camera.startPreview();
+	}
+	
+	public void startPreview() { camera.startPreview(); }
+
+	@Override
+	public void surfaceCreated(SurfaceHolder arg0) {
+		try
+        {
+                //Open the Camera in preview mode
+                this.camera = Camera.open();
+                this.camera.setPreviewDisplay(this.holder);
+        }
+        catch(IOException ioe)
+        {
+                ioe.printStackTrace(System.out);
+        }
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder arg0) {
+		// Surface will be destroyed when replaced with a new screen
+        //Always make sure to release the Camera instance
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+
+	}
+
+	public Camera getCamera()
+    {
+            return this.camera;
+    }
+
+}
