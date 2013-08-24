15ae75032b938206bcb4c4c8b4272a3d3284903d
Allen_Polak <apolak@systemsinmotion.com>
2013-08-24 14:00:58 -0400

    Added Android Context to ServiceServer

--- app/src/main/java/com/timetravellingtreasurechest/app/MainActivity.java
+++ app/src/main/java/com/timetravellingtreasurechest/app/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		ServiceServer.setAndroidContext(getApplicationContext());
 		ServiceServer.setFacialFeatureService(new AndroidFacialFeatureService(
 				this.getApplicationContext()));
 		ServiceServer
--- vision/src/main/java/com/timetravellingtreasurechest/services/ServiceServer.java
+++ vision/src/main/java/com/timetravellingtreasurechest/services/ServiceServer.java
@@ -1,7 +1,10 @@
 package com.timetravellingtreasurechest.services;
 
+import android.content.Context;
+
 public class ServiceServer {
 	private static IFacialFeatureService facialFeatureService;
+	private static Context context;
 	public static void setFacialFeatureService(IFacialFeatureService f) {
 		facialFeatureService = f;
 	}
@@ -15,5 +18,13 @@ public class ServiceServer {
 	}
 	public static IReportGeneratorService getReportGeneratorService() {
 		return reportGeneratorService;
+	}
+	
+	public static Context getAndroidContext() {
+		return context;
+	}
+	
+	public static void setAndroidContext(Context context) {
+		ServiceServer.context = context;
 	}
 }
